b19761ce7a7f263f76e9498064d1901d59172c57
Lucas Moreau-Lalaux <lucas.moreau-lalaux@ens-lyon.fr>
2025-12-10 13:52:16 +0100

    Variable path for importing the pulse sequences

--- src/pymodaq_plugins_pulsedmeasurement/extensions/pulsed_measurement_extension.py
+++ src/pymodaq_plugins_pulsedmeasurement/extensions/pulsed_measurement_extension.py
@@ -41,21 +41,12 @@ main_config = Config()
 plugin_config = PluginConfig()
 config_pymodaq = PyMoConfig()
 
-folder = "C:/Users/Aurore/pymodaq_pulse_sequences"
-
-print("OUI")
-
-
-def import_from_path(module_name, file_path):
-    """Import a module given its name and file path."""
-    spec = importlib.util.spec_from_file_location(module_name, file_path)
-    module = importlib.util.module_from_spec(spec)
-    sys.modules[module_name] = module
-    spec.loader.exec_module(module)
-    return module
-
-
-pulse_sequences_module = import_from_path(folder.split("/")[-1], folder)
+sys.path.append(
+    "/".join(plugin_config("Extension", "sequences_folder").split("\\")[:-1])
+)
+pulse_sequences_module = importlib.import_module(
+    plugin_config("Extension", "sequences_folder").split("\\")[-1]
+)
 
 
 PLOT_COLORS = [dict(color=color) for color in utils.plot_colors]
